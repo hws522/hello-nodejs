d22eb067786658a97c172b6e766e71117ce50f20
hws522 <sraphic@kakao.com>
2021-03-28 23:24:01 +0900

    chapter1 -> chapter2 수정

---
+++ chapter2/find-includes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const arr = ["node.js", "올인원"];
+
+const ret = arr.find((key) => key === "올인원");
+console.log(ret);
+
+// 올인원
+
+const res = arr.includes("node.js");
+console.log(res);
+
+// true
---
+++ chapter2/map-filter.js
@@ -0,0 +1,7 @@
+const a = [1, 2, 3];
+const b = a.map((x) => x + 1);
+console.log(b);
+
+const arr = [1, 2, 3];
+const f = arr.filter((x) => x > 1);
+console.log(f);
